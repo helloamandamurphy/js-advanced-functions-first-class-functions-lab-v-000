e902e8ef90c4fbdc9b2cc8a3e4175170108baae0
Amanda Murphy <helloamandamurphy@gmail.com>
2019-03-04 18:32:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function(drivers) {
-  return drivers[0], drivers[1];
+  return drivers
 }
 
 const returnLastTwoDrivers = function(drivers) {}
