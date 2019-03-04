ab0137ead750908a567cf387a20b1b62bebdfc3d
Amanda Murphy <helloamandamurphy@gmail.com>
2019-03-04 18:04:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file!
-function returnFirstTwoDrivers() {}
+const returnFirstTwoDrivers = function(drivers) {}
 
-function returnLastTwoDrivers() {}
+const returnLastTwoDrivers = function(drivers) {}
+
